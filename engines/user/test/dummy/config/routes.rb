92e68e7d7d5b55d4b19380825d25524a8ce8c751
David Henry <dw_henry@yahoo.com.au>
2014-03-22 18:32:27 +0000

    Remove test directory

--- engines/user/test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount User::Engine => "/user"
-end
--- engines/user/test/user_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, User
-  end
-end
