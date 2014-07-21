799452f11177af37e2e632f7a2846a4c7fc467ed
David Henry <dw_henry@yahoo.com.au>
2014-07-21 21:54:42 +0100

    Attempt to use message passing to update views..'

--- engines/demo/app/assets/javascripts/demo/app/games/gameDirective.js
+++ engines/demo/app/assets/javascripts/demo/app/games/gameDirective.js
@@ -14,15 +14,16 @@ angular.module('demoApp')
 
     directive.link = function(scope, elements, attr) {
 
-      var joinGame = function(gameID) {
-        gamesService.joinGame(gameID).then(function() {
-          alert('join')
+      var game = scope.game;
+      var joinGame = function() {
+        gamesService.joinGame(game.id).then(function() {
+          scope.$emit('addGame', {game: game})
         });
       };
 
-      var leaveGame = function(gameID) {
-        gamesService.leaveGame(gameID).then(function() {
-          alert('leave')
+      var leaveGame = function() {
+        gamesService.leaveGame(game.id).then(function() {
+          scope.$emit('removeGame', {game: game})
         });
       };
 
@@ -34,33 +35,72 @@ angular.module('demoApp')
 
     return directive;
   }])
-  .directive('gamesTableDetails', ['gamesService', 'accountService', function(gamesService, accountService) {
-    var user = accountService.getUser();;
-    var directive = {};
-
+  .directive('pendingGamesTableDetails', ['gamesService', 'accountService', function(gamesService, accountService) {
+    var directive = gamesTableDetails(gamesService, accountService);
+//    directive.require = '^gamesTableDetails';
     directive.restrict = 'AE';
-    directive.template = $('#gamesTableDetail').html();
-
-    directive.scope = {
-      games: "=games"
-    };
-
     directive.link = function(scope, elements, attr) {
-      scope.currentPlayer = function(gameID) {
-        var game = scope.games.filter(function(game) {
-          return (game.id == gameID);
-        })[0]
+      directive.linkAction(scope);
 
-        var player = game.players.filter(function(player) {
-          return player.id == user.id
-        })[0];
-
-        return player !== undefined
-      };
+      scope.$on('addGame', function(opt) {
+        scope.games.push(opt.targetScope.game);
+//        scope.$apply()
+      });
+      scope.$on('removeGame', function(opt) {
+        scope.games.splice(scope.games.indexOf(opt.targetScope.game), 1)
+//        scope.$apply()
+      });
     };
+    return directive;
+  }])
+  .directive('currentGamesTableDetails', ['gamesService', 'accountService', function(gamesService, accountService) {
+    var directive = gamesTableDetails(gamesService, accountService);
+//    directive.require = '^gamesTableDetails';
+    directive.restrict = 'AE';
 
+    directive.link = function(scope, element, attr) {
+      directive.linkAction(scope);
+
+      scope.$on('addGame', function(opt) {
+//        scope.games.push(opt.targetScope.game)
+//        scope.$apply()
+      });
+      scope.$on('removeGame', function (opt) {
+        var game = opt.targetScope.game;
+        if (game.players.count > 1)
+          scope.games.push(opt.targetScope.game)
+      });
+    };
     return directive;
   }]);
 
+var gamesTableDetails = function(gamesService, accountService) {
+  var user = accountService.getUser();
+  var directive = {};
+
+  directive.restrict = 'AE';
+  directive.template = $('#gamesTableDetail').html();
+
+  directive.scope = {
+    games: "=games"
+  };
+
+  directive.linkAction = function(scope, elements, attr) {
+
+    scope.currentPlayer = function(gameID) {
+      var game = scope.games.filter(function(game) {
+        return (game.id == gameID);
+      })[0];
+
+      var player = game.players.filter(function(player) {
+        return player.id == user.id
+      })[0];
+
+      return player !== undefined
+    };
+  };
+
+  return directive;
+}
 
 
